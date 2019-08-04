dcdc71e22565d11827121c9ee513546354765f2f
willieino <willieino@hotmail.com>
2019-08-04 12:56:46 -0700

    clean up code

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -33,16 +33,11 @@ def reconstruct_trip(tickets, length):
     route = []
     for i in range(length):
         hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-   
     found = False
     count = 0
     source = ""
-    while found is False:
-        print("Count: ", count)
-        destination = hash_table_retrieve(hashtable, tickets[count].source)
-        print(destination)
-        print(tickets[count].source)
-        
+    while found is False:       
+        destination = hash_table_retrieve(hashtable, tickets[count].source)       
         if tickets[count].source == "NONE":
             found = True
             route.append(destination)
@@ -50,30 +45,19 @@ def reconstruct_trip(tickets, length):
         count += 1
     count = 0
     found = False
-    #print("Destination", destination)
-    while destination is not "NONE":
-    #for i in range(length):
-        
+    
+    while found is False:      
         destination = hash_table_retrieve(hashtable, tickets[count].source)
-        if source == tickets[count].source:
+        if source == tickets[count].source:        
+            route.append(destination)
             if destination == "NONE":
                 return route
-            route.append(destination)
             source = destination
-            count = 0  
-        #elif destination == None:
-        #    return route
+            count = 0         
+        elif source == "NONE":       
+            return route
         else:
             count += 1
-            print(destination)       
-  
     return route
 
 
-#ticket_1 = Ticket("NONE", "PDX")
-#ticket_2 = Ticket("PDX", "DCA")
-#ticket_3 = Ticket("DCA", "NONE")
-#length = 2
-#tickets = [ticket_1, ticket_2, ticket_3]
-#new_route = reconstruct_trip(tickets, length)
-#print(new_route)
